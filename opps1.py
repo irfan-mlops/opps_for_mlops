cde8ba0587ec508f5f8d73b25b9ba161a29404d7
irfan <irfan@irfans-MacBook-Pro.local>
2026-08-06 05:36:32 +0900

    Class and object basic covered

---
+++ opps1.py
@@ -0,0 +1,23 @@
+class Employee:
+    # Special method in python, Magic methods or dunder methods.
+    def __init__(self):
+        print("Employee executing data/attributes.")
+        self.id = 101
+        self.name = "Irfan Muhammad"
+        self.salary = 100000
+        self.designation = "Software Engineer"
+        print("Employee data/attributes initialized.")
+
+    def travel(self):
+        print("Employee executing manually travel method.")
+        self.destination = "London"
+        print(f"Traveling to the {self.destination} office")
+    
+sam = Employee()
+print(sam.id)
+print(sam.name)
+print(sam.salary)
+print(sam.designation)
+
+# Calling the method manually.
+sam.travel() 
---
+++ rough.py
@@ -0,0 +1,10 @@
+ls = [1, 2, 3, 4, 5]
+str = "Hello, World!"
+my_int = 1555
+
+print(type(ls))
+print(type(str))
+print(type(my_int))
+
+ls.clear()
+print(ls)
